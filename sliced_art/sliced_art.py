d31d4b4c0e19627fe9de3b642cdc1f87e28a99bb
Don Kirkby <donkirkby@gmail.com>
2019-12-28 12:20:07 -0800

    Open an image file and display it.

--- sliced_art/sliced_art.py
+++ sliced_art/sliced_art.py
@@ -1,5 +1,7 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow
+
+from PySide2.QtGui import QImageReader, QPixmap
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QFileDialog
 
 from sliced_art.main_window import Ui_MainWindow
 
@@ -9,7 +11,24 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.scene = QGraphicsScene(self)
+        self.scene.addText('Hello, Scene!')
+        self.ui.graphicsView.setScene(self.scene)
         # self.ui.start.clicked.connect(self.on_start)
+        self.ui.actionExit.triggered.connect(self.close)
+        self.ui.actionOpen.triggered.connect(self.open_image)
+
+    def open_image(self):
+        formats = QImageReader.supportedImageFormats()
+        patterns = (f'*.{fmt.data().decode()}' for fmt in formats)
+        image_filter = f'Images ({" ".join(patterns)})'
+        file_name, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open an image file.",
+            filter=image_filter,
+            options=QFileDialog.DontUseNativeDialog)
+        pixmap = QPixmap(file_name)
+        self.scene.addPixmap(pixmap)
 
 
 def main():
